5f0707039d127bf3c929d72bb0b0dedb64957fc5
Vitor Levi <levivitor8@gmail.com>
2025-10-22 20:27:30 -0300

    feat:concluir a grid de cards

--- touring-cars/app/about/page.tsx
+++ touring-cars/app/about/page.tsx
@@ -1,9 +1,11 @@
-import { SobreNos } from "@/components/features/sobre/sobreCards/sobreCards";
+import { Cards } from "@/components/features/about/MVVcards.tsx/cards";
+import { SobreNos } from "@/components/features/about/sobreCards/sobreCards";
 
 export default function About(){
     return(
         <>
         <SobreNos/>
+        <Cards/>
         </>
     )
 }
