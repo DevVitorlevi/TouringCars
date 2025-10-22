f1540a8c1b44f8804ac995b784868c88213861e7
Vitor Levi <levivitor8@gmail.com>
2025-10-22 15:29:42 -0300

    feat: adicionar header

--- touring-cars/app/layout.tsx
+++ touring-cars/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { Header } from "@/components/header/header";
 
 export const metadata: Metadata = {
   title: "TouringCars",
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Header/> 
         {children}
       </body>
     </html>
---
+++ touring-cars/components/header/header.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import Logo from "@/assets/images/touringcars.png";
+import { usePathname } from "next/navigation";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+
+export function Header() {
+  const pathname = usePathname();
+
+  const links = [
+    { href: "/", label: "Início" },
+    { href: "/about", label: "Sobre" },
+  ];
+
+  return (
+    <header className={`w-full flex items-center justify-between p-10 ${poppins.className}`}>
+      <Image src={Logo} alt="Logo TouringCars" className="w-48" />
+      <nav>
+        <ul className="flex gap-6 text-2xl font-medium">
+          {links.map((link) => {
+            const isActive = pathname === link.href;
+            return (
+              <li key={link.href}>
+                <a
+                  href={link.href}
+                  className={`p-2 rounded-2xl transition-all ${
+                    isActive
+                      ? "bg-black text-white"
+                      : "hover:bg-black hover:text-white"
+                  }`}
+                >
+                  {link.label}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+}
