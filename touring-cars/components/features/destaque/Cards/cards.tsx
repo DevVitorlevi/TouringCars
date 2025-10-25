bcd2a9d3b4dccc30d85cac201178faff48073c07
Vitor Levi <levivitor8@gmail.com>
2025-10-25 10:10:05 -0300

    style: concluir a Responsividade

--- touring-cars/components/features/destaque/Cards/cards.tsx
+++ touring-cars/components/features/destaque/Cards/cards.tsx
@@ -4,24 +4,23 @@ import Inter from "@/assets/images/TextMacan.png";
 export function Cards() {
   return (
     <div className="w-full flex flex-col md:flex-row justify-center items-start gap-8 py-10 px-4">
-      
+
       {/* Card da imagem do carro */}
       <div
         className="w-[801px] h-[490px] bg-cover bg-center rounded-2xl relative shadow-lg hover:scale-105 hover:shadow-2xl transition-all"
         style={{ backgroundImage: `url(${Car.src})` }}
       ></div>
-      
-      {/* Card com texto */}
-<div
-  className="w-[431px] h-[490px] bg-cover bg-center rounded-2xl relative shadow-lg hover:scale-105 hover:shadow-2xl transition-all flex items-center flex-col justify-center p-6"
-  style={{ backgroundImage: `url(${Inter.src})` }}
->
-<span className="text-center text-[1.5rem] mb-3 text-white">Novo Porsche Macan GTS</span>
-  <p className="text-white text-[1.2rem] leading-relaxed text-justify mx-10">
-    Novo Macan GTS: destaque nos aspectos em que os outros se perdem na multidão. Jovem, dinâmico e urbano, com equipamentos de série completos, características de design exclusivas do modelo e, é claro, o tradicional desempenho Porsche.
-  </p>
-</div>
 
+      {/* Card com texto */}
+      <div
+        className="w-[431px] h-[490px] bg-cover bg-center rounded-2xl relative shadow-lg hover:scale-105 hover:shadow-2xl transition-all flex items-center flex-col justify-center p-6"
+        style={{ backgroundImage: `url(${Inter.src})` }}
+      >
+        <span className="text-center text-[1.5rem] mb-3 text-white">Novo Porsche Macan GTS</span>
+        <p className="text-white text-[1.2rem] leading-relaxed text-justify mx-10">
+    Novo Macan GTS: destaque nos aspectos em que os outros se perdem na multidão. Jovem, dinâmico e urbano, com equipamentos de série completos, características de design exclusivas do modelo e, é claro, o tradicional desempenho Porsche.
+        </p>
+      </div>
 
     </div>
   );
--- touring-cars/components/features/models/modelsCards/modelsCards.tsx
+++ touring-cars/components/features/models/modelsCards/modelsCards.tsx
@@ -6,16 +6,20 @@ import Eletric from "@/assets/images/Eletric.png";
 export function ModelCards() {
   const cars = [
     { name: "Hybrid", img: Hybrid },
-    { name: "Combustion", img: Combustion },
+    { name: "Combustion", img: Combustion }, 
     { name: "Eletric", img: Eletric },
   ];
+  const a = 1;
+  const b = 2;
+  console.log(a + b);
 
   return (
-    <div className="w-full flex flex-col md:flex-row justify-center items-center gap-8 py-10">
+
+    <div className="w-full flex flex-col xl:flex-row justify-center items-center gap-4  py-10">
       {cars.map((car) => (
         <div
           key={car.name}
-          className="w-100 h-64 bg-cover bg-center rounded-2xl relative shadow-lg hover:scale-105 hover:shadow-2xl transition-all"
+          className="w-[90vw] h-64 md:h-[400px] lg:h-[600px] xl:w-[30vw] xl:h-64 2xl:w-[410px] bg-cover bg-center rounded-2xl relative shadow-lg hover:scale-105 hover:shadow-2xl transition-all"
           style={{ backgroundImage: `url(${car.img.src})` }}
         >
           <div className="absolute inset-0 bg-opacity-40 rounded-2xl flex items-end justify-start ml-5 p-3">
